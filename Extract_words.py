19f4d75d65178552404004f5bb242f19e1428d71
Defjia <code@defjia.top>
2018-09-13 21:27:21 +0800

    Add on 13th Spt

---
+++ Extract_words.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+
+
+if __name__ == '__main__':
+    file = 'data/ldoce6_book_picrefs_cn.xml'
+    fo = open(file, 'r+', encoding="utf8")
+    context = fo.read()
+    soup = BeautifulSoup(context, "html5lib")
+    entries = soup.find_all('entry')
+    middle = list()
+    final = list()
+    for entry in entries:
+        if 'type' in entry.attrs and entry['type'] == 'encyc':
+            proncodes = entry.find_all('PronCodes')
+            hwd = entry.find('HWD')
+            if len(proncodes) > 0:
+                middle.append(hwd)
+            else:
+                final.append(hwd)
+    res = ''
+    for elem in final:
+        res += hwd.text + ',\n'
+    fo = open("data/result.csv", "a+")
+    fo.write(res)
+    fo.close()
