68606386ea697eab90195f6dd1ca04811a871aff
Defjia <code@defjia.top>
2020-02-25 22:25:50 +0800

    :bento:Updating codes

--- Extract_words.py
+++ Extract_words.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 
 if __name__ == '__main__':
-    file = 'data/ldoce6_book_picrefs_cn.xml'
+    file = '.data/ldoce6_book_picrefs_cn.xml'
     fo = open(file, 'r+', encoding="utf8")
     context = fo.read()
     soup = BeautifulSoup(context, "html5lib")
@@ -11,15 +11,15 @@ if __name__ == '__main__':
     final = list()
     for entry in entries:
         if 'type' in entry.attrs and entry['type'] == 'encyc':
-            proncodes = entry.find_all('PronCodes')
-            hwd = entry.find('HWD')
+            proncodes = entry.find_all('proncodes')
+            hwd = entry.find('hwd')
             if len(proncodes) > 0:
                 middle.append(hwd)
             else:
                 final.append(hwd)
     res = ''
     for elem in final:
-        res += hwd.text + ',\n'
-    fo = open("data/result.csv", "a+")
+        res += elem.text + ',\n'
+    fo = open(".data/result.csv", "a+")
     fo.write(res)
     fo.close()
